f8f8645632a690fede6c0485e3085c46d63ad4f0
Your Name <you@example.com>
2024-08-31 15:10:14 +0330

    FixUserFillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,10 +20,8 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
+    protected $guarded = [
+        'id',
     ];
 
     /**
