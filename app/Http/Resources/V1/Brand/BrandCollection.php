a51b2284c45dae79bebcbc33f065d48cfb40ec55
mehran <mehransobhani77@gmail.com>
2024-09-06 15:35:21 +0330

    CategoryCollection

--- app/Http/Resources/V1/Brand/BrandCollection.php
+++ app/Http/Resources/V1/Brand/BrandCollection.php
@@ -10,8 +10,19 @@ class BrandCollection extends ResourceCollection
 {
     public function toArray(Request $request): array
     {
+        if ($this->resource instanceof LengthAwarePaginator)
         return [
             'data' => $this->collection,
+            'meta' => [
+                'total' => $this->total(),
+                'current_page' => $this->currentPage(),
+                'last_page' => $this->lastPage(),
+                'per_page' => $this->perPage(),
+            ],
         ];
+
+    return [
+        'data' => $this->collection,
+    ];
     }
 }
