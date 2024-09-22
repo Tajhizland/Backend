563d5318bfb8654ac4f8e65bebf2993281d0d6d9
Your Name <you@example.com>
2024-09-22 11:10:31 +0330

    Listing

--- app/Services/Filter/FilterService.php
+++ app/Services/Filter/FilterService.php
@@ -23,42 +23,44 @@ class FilterService implements FilterServiceInterface
 
     public function apply($productQuery, $filters)
     {
-        foreach ($filters as $filter => $value) {
-            /** Dynamic Filters **/
+        if($filters) {
+            foreach ($filters as $filter => $value) {
+                /** Dynamic Filters **/
 
-            if (is_array($value)) {
-                /** Example : filter[1][]=10  (filter[filter_id][]=filter_item_id) */
-                $this->productRepository->otherFilter($filter, $value, $productQuery);
-                continue;
+                if (is_array($value)) {
+                    /** Example : filter[1][]=10  (filter[filter_id][]=filter_item_id) */
+                    $this->productRepository->otherFilter($filter, $value, $productQuery);
+                    continue;
+                }
+
+                /** Static Filters **/
+
+                if ($filter == "minPrice") {
+                    /** Example : filter[minPrice]=10 */
+                    $this->productRepository->minPriceFilter($productQuery, $value);
+                    continue;
+                }
+
+                if ($filter == "maxPrice") {
+                    /** Example : filter[maxPrice]=10 */
+                    $this->productRepository->maxPriceFilter($productQuery, $value);
+                    continue;
+                }
+
+                if ($filter == "name" && $value != "") {
+                    /** Example : filter[name]=abc */
+                    $this->productRepository->nameFilter($productQuery, $value);
+                    continue;
+                }
+
+                if ($filter == "just_has_stock" && $value == 1) {
+                    /** Example : filter[just_has_stock]=1 */
+                    $this->productRepository->hasStockFilter($productQuery);
+                    continue;
+                }
+
+
             }
-
-            /** Static Filters **/
-
-            if ($filter == "minPrice") {
-                /** Example : filter[minPrice]=10 */
-                $this->productRepository->minPriceFilter($productQuery, $value);
-                continue;
-            }
-
-            if ($filter == "maxPrice") {
-                /** Example : filter[maxPrice]=10 */
-                $this->productRepository->maxPriceFilter($productQuery, $value);
-                continue;
-            }
-
-            if ($filter == "name" && $value != "") {
-                /** Example : filter[name]=abc */
-                $this->productRepository->nameFilter($productQuery, $value);
-                continue;
-            }
-
-            if ($filter == "just_has_stock" && $value == 1) {
-                /** Example : filter[just_has_stock]=1 */
-                $this->productRepository->hasStockFilter($productQuery);
-                continue;
-            }
-
-
         }
         return $productQuery;
     }
