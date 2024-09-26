044d53d324c5417738eb140bee0630002957cd5f
mehran <mehransobhani77@gmail.com>
2024-09-26 15:02:46 +0330

    HomepageResponse

--- app/Http/Resources/V1/HomePage/HomePageResource.php
+++ app/Http/Resources/V1/HomePage/HomePageResource.php
@@ -6,8 +6,8 @@ use App\Http\Resources\V1\Concept\ConceptCollection;
 use App\Http\Resources\V1\HomepageCategory\HomepageCategoryCollection;
 use App\Http\Resources\V1\PopularCategory\PopularCategoryCollection;
 use App\Http\Resources\V1\PopularProduct\PopularProductCollection;
-use App\Http\Resources\V1\Product\SimpleProduct\SimpleProductCollection;
 use App\Http\Resources\V1\Slider\SliderCollection;
+use App\Http\Resources\V1\SpecialProduct\SpecialProductCollection;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -21,7 +21,7 @@ class HomePageResource extends JsonResource
             "homepageCategories" => new HomepageCategoryCollection($this["homepageCategories"]),
             "sliders" => new SliderCollection($this["sliders"]),
             "concepts" => new ConceptCollection($this["concepts"]),
-            "specialProducts" => new SimpleProductCollection($this["specialProducts"]),
+            "specialProducts" => new SpecialProductCollection($this["specialProducts"]),
         ];
     }
 }
