beb73e70e5a9938d1dfffb0722f44affd4caa180
Your Name <you@example.com>
2024-10-07 11:04:14 +0330

    providerFix

--- app/Http/Resources/V1/Comment/CommentCollection.php
+++ app/Http/Resources/V1/Comment/CommentCollection.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources\V1\Comment;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class CommentCollection extends ResourceCollection
 {
