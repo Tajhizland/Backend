be67f27b06fdf64a30171aea57ee13664e6c29ad
Your Name <you@example.com>
2024-10-07 12:30:30 +0330

    FilterRequest

--- app/Http/Requests/V1/Admin/Filter/SetFilterRequest.php
+++ app/Http/Requests/V1/Admin/Filter/SetFilterRequest.php
@@ -12,7 +12,7 @@ class SetFilterRequest extends FormRequest
             "category_id" => "required|exists:categories,id",
             "filter.*.id"=>"numeric|exists:filters,id|nullable",
             "filter.*.item.*.id"=>"numeric|exists:filter_items,id|nullable",
-            "filter.*.item.*.value"=>"string|required",
+            "filter.*.item.*.name"=>"string|required",
             "filter.*.item.*.status"=>"numeric|in:0,1|required",
         ];
     }
