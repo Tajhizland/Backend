8d23b1f25267905a8a6a318a79ed82a5d83bf0d2
Your Name <you@example.com>
2024-10-07 12:01:52 +0330

    CommentProduct

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -17,12 +17,12 @@ class Comment extends Model
         ];
     }
 
-    protected function user(): BelongsTo
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
-    protected function product(): BelongsTo
+    public function product(): BelongsTo
     {
         return $this->belongsTo(Product::class);
     }
--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function (Request $request) {
    echo "Tajhizland Backend Api";
 });
-Route::get("option/get/{id}", [\App\Http\Controllers\V1\Admin\ProductController::class, "getOption"]);
-Route::get("filter/get/{id}", [\App\Http\Controllers\V1\Admin\ProductController::class, "getFilter"]);
