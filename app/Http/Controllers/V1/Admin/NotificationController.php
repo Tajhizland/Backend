209e53bd7ff9f155f6c02b70fe289e658630d437
Your Name <you@example.com>
2024-10-09 16:29:55 +0330

    NotificationDataTable

--- app/Http/Controllers/V1/Admin/NotificationController.php
+++ app/Http/Controllers/V1/Admin/NotificationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\V1\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\Notification\NotificationCollection;
 use App\Services\Notification\NotificationService;
 
 class NotificationController extends Controller
@@ -11,10 +12,10 @@ class NotificationController extends Controller
     { }
     public function unSeen()
     {
-        return $this->dataResponseCollection($this->notificationService->getUnSeen());
+        return $this->dataResponseCollection(new NotificationCollection($this->notificationService->getUnSeen()));
     }
     public function dataTable()
     {
-        return $this->dataResponseCollection($this->notificationService->dataTable());
+        return $this->dataResponseCollection(new NotificationCollection($this->notificationService->dataTable()));
     }
 }
