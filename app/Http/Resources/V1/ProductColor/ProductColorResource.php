ad67b3a11e805e12d6e0635d60bb675e3b861294
Your Name <you@example.com>
2024-10-14 14:55:35 +0330

    ProductStatus

--- app/Http/Resources/V1/ProductColor/ProductColorResource.php
+++ app/Http/Resources/V1/ProductColor/ProductColorResource.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Resources\V1\ProductColor;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
-use Illuminate\Support\Facades\Auth;
 use Morilog\Jalali\Jalalian;
 
 /** @mixin \App\Models\ProductColor */
@@ -12,17 +12,26 @@ class ProductColorResource extends JsonResource
 {
     public function toArray(Request $request): array
     {
-            return [
+        $statusLabel = "";
+        if (!$this->stock || $this->stock->stock < 1 || !$this->price || $this->price <= 0) {
+            $statusLabel = "disable";
+        } else if ($this->price->discount > 0) {
+            $statusLabel = "discount";
+        } else if ($this->created_at > Carbon::now()->subWeek()) {
+            $statusLabel = "new";
+        }
+        return [
             'id' => $this->id,
             'product_id' => $this->product_id,
             'color_name' => $this->color_name,
             'color_code' => $this->color_code,
             'delivery_delay' => $this->delivery_delay,
             'status' => $this->status,
+            'statusLabel' => $statusLabel,
             'price' => $this->price?->price,
             'discount' => $this->price?->discount,
-            'stock' => $this->stock?->stock??0,
-             'created_at' => Jalalian::fromDateTime($this->created_at)->format('Y/m/d H:i:s'),
+            'stock' => $this->stock?->stock ?? 0,
+            'created_at' => Jalalian::fromDateTime($this->created_at)->format('Y/m/d H:i:s'),
             'updated_at' => Jalalian::fromDateTime($this->updated_at)->format('Y/m/d H:i:s'),
         ];
     }
