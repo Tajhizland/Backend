be692a88a9eabb57722cc105b6399e972a9c4ba5
mehran <mehransobhani77@gmail.com>
2024-10-17 13:00:53 +0330

    Fix

--- app/Models/Faq.php
+++ app/Models/Faq.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
+use App\Enums\FaqStatus;
 use Illuminate\Database\Eloquent\Model;
 
 class Faq extends Model
 {
     protected $guarded=["id"];
+
+    public function scopeActive(Builder $query): Builder
+    {
+        return $query->where("status", FaqStatus::Active->value);
+    }
 
 }
