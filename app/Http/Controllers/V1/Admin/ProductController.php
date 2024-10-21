c998deaf4659858ab3ca1728d436b422ff223794
Your Name <you@example.com>
2024-10-21 13:00:35 +0330

    ProductVideos

--- app/Http/Controllers/V1/Admin/ProductController.php
+++ app/Http/Controllers/V1/Admin/ProductController.php
@@ -110,6 +110,8 @@ class ProductController extends Controller
     }
     public function setVideo(SetVideoRequest $request)
     {
+        return $this->successResponse(Lang::get("action.upload", ["attr" => Lang::get("attr.file")]));
+
         UploadVideoJob::dispatch($request->get("productId"),$request->file("file"),$request->get("type"));
         return $this->successResponse(Lang::get("action.upload", ["attr" => Lang::get("attr.file")]));
     }
