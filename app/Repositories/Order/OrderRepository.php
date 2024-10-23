8da91c703a1a03b6955f211905704ced8eeeca6a
Your Name <you@example.com>
2024-10-23 10:30:11 +0330

    FixOrder

--- app/Repositories/Order/OrderRepository.php
+++ app/Repositories/Order/OrderRepository.php
@@ -58,6 +58,7 @@ class OrderRepository extends BaseRepository implements OrderRepositoryInterface
 
     public function createOrder($user_id, $order_info_id, $price, $delivery_price, $final_price, $status, $payment_method, $delivery_method, $order_date, $delivery_date, $tracking_number)
     {
+        $payment_method=$payment_method??config("settings.default_gateway");
         return $this->create(
             [
                 "user_id" => $user_id,
