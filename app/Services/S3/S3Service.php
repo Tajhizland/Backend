f86c8b7649aa3d3d5c5a3117bb730406bfc397c3
Your Name <you@example.com>
2024-11-02 16:02:55 +0330

    fix

--- app/Services/S3/S3Service.php
+++ app/Services/S3/S3Service.php
@@ -11,7 +11,9 @@ class S3Service implements S3ServiceInterface
         if ($fileName == "")
             $fileName = time() . "_" . rand(10000, 99999) . '.' . $file->getClientOriginalExtension();
         $filePath = $path . '/' . $fileName;
-        Storage::disk('s3')->put($filePath, file_get_contents($file));
+        $t=Storage::disk('s3')->put($filePath, file_get_contents($file));
+        var_dump($t);
+        die();
         return $fileName;
     }
 
