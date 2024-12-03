e600e971ec4dc95c8f183efe1c55cdee3d09eda3
Your Name <you@example.com>
2024-12-03 12:55:03 +0330

    Contact

--- app/Http/Resources/V1/Contact/ContactResource.php
+++ app/Http/Resources/V1/Contact/ContactResource.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Resources\V1\Contact;
 
+use App\Http\Resources\V1\City\CityResource;
+use App\Http\Resources\V1\Province\ProvinceResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Morilog\Jalali\Jalalian;
@@ -16,6 +18,11 @@ class ContactResource extends JsonResource
             'name' => $this->name,
             'email' => $this->email,
             'message' => $this->message,
+            'city_id' => $this->city_id,
+            'concept' => $this->concept,
+            'province_id' => $this->province_id,
+            'city' => new CityResource($this->city),
+            'province' => new ProvinceResource($this->province),
             'created_at' => Jalalian::fromDateTime($this->created_at)->format('Y/m/d H:i:s'),
             'updated_at' => Jalalian::fromDateTime($this->updated_at)->format('Y/m/d H:i:s'),
 
