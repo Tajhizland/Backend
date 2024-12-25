91ef1eee1363195fe96b74bed276db21b96769f4
Your Name <you@example.com>
2024-12-25 16:50:14 +0330

    Service

--- app/Services/Guaranty/GuarantyServiceInterface.php
+++ app/Services/Guaranty/GuarantyServiceInterface.php
@@ -14,5 +14,5 @@ interface GuarantyServiceInterface
 
     public function store($name, $free, $description, $icon, $status ,$url);
 
-    public function update($id, $free, $name, $description, $icon, $status,$url);
+    public function update($id,  $name, $free,$description, $icon, $status,$url);
 }
