9e85e9dafbabf73de8c47110adf8030a4d0fa406
Your Name <you@example.com>
2025-02-09 13:36:52 +0330

    CategoryBlog

--- app/Http/Controllers/V1/Shop/NewsController.php
+++ app/Http/Controllers/V1/Shop/NewsController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers\V1\Shop;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\Banner\BannerCollection;
+use App\Http\Resources\V1\BlogCategory\BlogCategoryCollection;
 use App\Http\Resources\V1\News\NewsCollection;
 use App\Http\Resources\V1\News\NewsResource;
 use App\Services\Banner\BannerServiceInterface;
+use App\Services\BlogCategory\BlogCategoryServiceInterface;
 use App\Services\New\NewServiceInterface;
 use Illuminate\Http\Request;
 
@@ -14,21 +16,24 @@ class NewsController extends Controller
 {
     public function __construct
     (
-        private  BannerServiceInterface $bannerService ,
-        private NewServiceInterface $newService
+        private BannerServiceInterface       $bannerService,
+        private NewServiceInterface          $newService,
+        private BlogCategoryServiceInterface $blogCategoryService
     )
     {
     }
 
     public function paginate()
     {
-        $banners=new BannerCollection($this->bannerService->getBlogBanner());
-        $listing=new NewsCollection($this->newService->activePaginate());
-        $lastPost=new NewsCollection($this->newService->getLastPost());
+        $banners = new BannerCollection($this->bannerService->getBlogBanner());
+        $listing = new NewsCollection($this->newService->activePaginate());
+        $lastPost = new NewsCollection($this->newService->getLastPost());
+        $category = new BlogCategoryCollection($this->blogCategoryService->list());
         return $this->dataResponse([
             "listing" => $listing,
             "banner" => $banners,
             "lastPost" => $lastPost,
+            "category" => $category,
         ]);
     }
 
