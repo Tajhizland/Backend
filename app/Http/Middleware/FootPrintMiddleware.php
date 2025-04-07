798dfc8854c7b3424bc7d6771838802a51fc8f4b
Your Name <you@example.com>
2025-04-07 16:34:06 +0330

    FootPrint

--- app/Http/Middleware/FootPrintMiddleware.php
+++ app/Http/Middleware/FootPrintMiddleware.php
@@ -6,11 +6,23 @@ use App\Models\Footprint;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Laravel\Sanctum\PersonalAccessToken;
 
 class FootPrintMiddleware
 {
     public function handle(Request $request, Closure $next)
     {
+        if (!auth()->check()) {
+            // اگر توکن در Authorization header هست
+            $token = $request->bearerToken();
+            if ($token) {
+                $accessToken = PersonalAccessToken::findToken($token);
+                if ($accessToken) {
+                    Auth::login($accessToken->tokenable);
+                }
+            }
+        }
+
         $page = $request->path();
         $userIp = $request->ip();
         $userId = auth()->check() ? auth()->id() : null;
