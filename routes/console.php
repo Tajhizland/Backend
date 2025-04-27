2447d55dcaee6ba94b40d2ac56c1f7a157b45acb
Your Name <you@example.com>
2025-04-27 15:39:19 +0330

    Backup

--- routes/console.php
+++ routes/console.php
@@ -7,4 +7,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command('backup:run')->daily()->at('11:00');
+Schedule::command('backup:run')->daily()->at('15:41');
