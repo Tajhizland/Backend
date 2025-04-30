d6a540808ebe351988591c902498c32a0d6f3aa3
Mehran <mehransobhani77@gmail.com>
2025-04-30 21:32:47 +0330

    HomepageVlogApi

--- app/Models/HomepageVlog.php
+++ app/Models/HomepageVlog.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class HomepageVlog extends Model
 {
+    protected $guarded = ["id"];
+
     public function vlog(): BelongsTo
     {
         return $this->belongsTo(Vlog::class);
