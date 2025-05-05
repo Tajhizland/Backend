f4b90cec0d2bf5983cff8d6f8a095df036493623
Mehran <mehransobhani77@gmail.com>
2025-05-05 19:00:19 +0330

    Children

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -40,7 +40,7 @@ class Category extends Model
     }
     public function children()
     {
-        return $this->hasMany(Category::class, 'parent_id');
+        return $this->hasMany(Category::class, 'parent_id')->where("status",1);
     }
 
 
