a3702621f5f12933290f4efcaeb171a76f482379
Mehran <mehransobhani77@gmail.com>
2025-05-14 18:58:37 +0330

    FixFindByUrl

--- app/Repositories/Product/ProductRepository.php
+++ app/Repositories/Product/ProductRepository.php
@@ -19,7 +19,7 @@ class ProductRepository extends BaseRepository implements ProductRepositoryInter
 
     public function findByUrl($url)
     {
-        return $this->model::withActiveColor()->active()->where("url", $url)->first();
+        return $this->model::withActiveColor()->whereHas("activeProductColors")->active()->where("url", $url)->first();
     }
 
 
