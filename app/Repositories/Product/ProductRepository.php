e03692a459b2fdfc2fe8d0f30da639c9bf3735d7
Your Name <you@example.com>
2025-05-24 11:45:21 +0330

    GroupProduct

--- app/Repositories/Product/ProductRepository.php
+++ app/Repositories/Product/ProductRepository.php
@@ -20,7 +20,7 @@ class ProductRepository extends BaseRepository implements ProductRepositoryInter
     public function findByUrl($url)
     {
         return $this->model::withActiveColor()
-            ->with(["groupItems","groupItems.value"])
+            ->with(["groupItems","groupItems.product","groupItems.value"])
             ->whereHas("activeProductColors")
             ->active()
             ->where("url", $url)
