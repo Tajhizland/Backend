505a6f15496d91dc118b7f6de089bcd06dd10465
Mehran <mehransobhani77@gmail.com>
2025-05-27 20:12:54 +0330

    chargeWallet

--- app/Repositories/WalletTransaction/WalletTransactionRepository.php
+++ app/Repositories/WalletTransaction/WalletTransactionRepository.php
@@ -4,9 +4,8 @@ namespace App\Repositories\WalletTransaction;
 
 use App\Models\WalletTransaction;
 use App\Repositories\Base\BaseRepository;
-use App\Services\WalletTransaction\WalletTransactionServiceInterface;
 
-class WalletTransactionRepository extends BaseRepository implements WalletTransactionServiceInterface
+class WalletTransactionRepository extends BaseRepository implements WalletTransactionRepositoryInterface
 {
     public function __construct(WalletTransaction $model)
     {
