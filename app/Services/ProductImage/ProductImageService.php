2ba7b75813822f624d0f8c850289e4e952d1113e
Mehran <mehransobhani77@gmail.com>
2025-07-14 19:23:04 +0330

    ProductImage

--- app/Services/ProductImage/ProductImageService.php
+++ app/Services/ProductImage/ProductImageService.php
@@ -24,7 +24,7 @@ class ProductImageService implements ProductImageServiceInterface
     public function upload($productId, $images )
     {
         foreach ($images as $image) {
-            $imagePath = $this->s3Service->upload($image, "product")
+            $imagePath = $this->s3Service->upload($image, "product");
             $_800X = $this->imageResizeService->resize($image, 800, 800);
             $this->s3Service->upload2($_800X, "product/800", $imagePath);
             $_300X = $this->imageResizeService->resize($image, 300, 300);
