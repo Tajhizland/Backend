79e1157d97720ac40f01cf38c01d8bbc27a92b4a
Mehran <mehransobhani77@gmail.com>
2025-09-21 18:51:52 +0330

    Poster

--- app/Http/Controllers/V1/Shop/SampleController.php
+++ app/Http/Controllers/V1/Shop/SampleController.php
@@ -3,16 +3,20 @@
 namespace App\Http\Controllers\V1\Shop;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\Poster\PosterResource;
 use App\Http\Resources\V1\Sample\SampleResource;
 use App\Http\Resources\V1\SampleImage\SampleImageCollection;
 use App\Http\Resources\V1\SampleVideo\SampleVideoCollection;
+use App\Repositories\Poster\PosterRepositoryInterface;
 use App\Services\Sample\SampleServiceInterface;
 
 class SampleController extends Controller
 {
     public function __construct
     (
-        private SampleServiceInterface $sampleService
+        private SampleServiceInterface    $sampleService,
+        private PosterRepositoryInterface $posterRepository,
+
     )
     {
     }
@@ -22,9 +26,11 @@ class SampleController extends Controller
         $info = $this->sampleService->find();
         $image = $this->sampleService->getImages();
         $video = $this->sampleService->getVideos();
+        $poster = $this->posterRepository->findOrFail(1);
 
         return $this->dataResponse([
             "info" => new SampleResource($info),
+            "poster" => new PosterResource($poster),
             "image" => new SampleImageCollection($image),
             "video" => new SampleVideoCollection($video)
         ]);
