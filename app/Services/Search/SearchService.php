b61c5c1567873d9be1e782fc166425187e2f0d3e
Mehran <mehransobhani77@gmail.com>
2025-09-27 19:46:32 +0330

    SearchVlog

--- app/Services/Search/SearchService.php
+++ app/Services/Search/SearchService.php
@@ -2,6 +2,8 @@
 
 namespace App\Services\Search;
 
+use App\Http\Resources\V1\Product\ProductCollection;
+use App\Http\Resources\V1\Vlog\VlogCollection;
 use App\Repositories\Category\CategoryRepositoryInterface;
 use App\Repositories\Product\ProductRepositoryInterface;
 use App\Repositories\Vlog\VlogRepositoryInterface;
@@ -19,7 +21,7 @@ class SearchService implements SearchServiceInterface
     {
         $products = $this->productRepository->search($query);
         $vlogs = $this->vlogRepository->searchQuery($query);
-        return ["products" => $products, "vlogs" => $vlogs];
+        return ["products" => ProductCollection::make($products), "vlogs" => VlogCollection::make($vlogs)];
     }
 
     public function searchPaginate($query)
