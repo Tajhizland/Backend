60f85c8a3f174f181560131a3ba0c77ae370a83d
Mehran <mehransobhani77@gmail.com>
2025-09-29 19:35:56 +0330

    Asia/Tehran

--- config/app.php
+++ config/app.php
@@ -65,8 +65,7 @@ return [
     |
     */
 
-    'timezone' => env('APP_TIMEZONE', 'Asia/Tehran'),
-
+    'timezone' => 'Asia/Tehran',
     /*
     |--------------------------------------------------------------------------
     | Application Locale Configuration
