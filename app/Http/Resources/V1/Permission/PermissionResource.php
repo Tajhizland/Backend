106d4cd2437698d0199e2aec6969eaf97b130f93
Mehran <mehransobhani77@gmail.com>
2025-10-04 07:54:43 +0330

    RoleRell

--- app/Http/Resources/V1/Permission/PermissionResource.php
+++ app/Http/Resources/V1/Permission/PermissionResource.php
@@ -14,6 +14,7 @@ class PermissionResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
+            'value' => $this->value,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
