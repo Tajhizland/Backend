e99042b8f2c72fb05873618ae048474654d972ca
Mehran <mehransobhani77@gmail.com>
2025-11-04 18:30:21 +0330

    HoldOrderMessage

--- app/Services/OnHoldOrder/OnHoldOrderService.php
+++ app/Services/OnHoldOrder/OnHoldOrderService.php
@@ -5,16 +5,19 @@ namespace App\Services\OnHoldOrder;
 use App\Enums\OrderStatus;
 use App\Repositories\OnHoldOrder\OnHoldOrderRepositoryInterface;
 use App\Repositories\Order\OrderRepositoryInterface;
+use App\Services\Sms\SmsServiceInterface;
 use Illuminate\Support\Facades\Gate;
 
 class OnHoldOrderService implements OnHoldOrderServiceInterface
 {
     public function __construct(
         private OnHoldOrderRepositoryInterface $onHoldOrderRepository,
-        private  OrderRepositoryInterface $orderRepository
+        private SmsServiceInterface            $smsService,
+        private OrderRepositoryInterface       $orderRepository
     )
     {
     }
+
     public function findById($id)
     {
         return $this->onHoldOrderRepository->findOrFail($id);
@@ -29,23 +32,29 @@ class OnHoldOrderService implements OnHoldOrderServiceInterface
     {
         $onHoldOrder = $this->onHoldOrderRepository->findOrFail($id);
         Gate::authorize("delete", $onHoldOrder);
-        $this->orderRepository->setStatus($onHoldOrder->order,OrderStatus::Unpaid->value);
+        $this->orderRepository->setStatus($onHoldOrder->order, OrderStatus::Unpaid->value);
         return $this->onHoldOrderRepository->delete($onHoldOrder);
     }
 
     public function setAccept($id)
     {
+        $message = "سفارش معلق شما در تجهیزلند توسط مدیریت تایید شد . شما اکنون میتوانید با پرداخت مبلغ سفارش ,  سفارش خود را تکمیل کنید
+        https://tajhizland.com/account-order-on-hold";
         $onHoldOrder = $this->onHoldOrderRepository->findOrFail($id);
         Gate::authorize("update", $onHoldOrder);
-        $this->orderRepository->setStatus($onHoldOrder->order,OrderStatus::Accepted->value);
+        $this->orderRepository->setStatus($onHoldOrder->order, OrderStatus::Accepted->value);
+        $this->smsService->send($onHoldOrder->order->orderInfo->mobile, $message);
         return $this->onHoldOrderRepository->setAccept($onHoldOrder);
     }
 
     public function setReject($id)
     {
+        $message = "سفارش معلق شما در تجهیزلند توسط مدیریت رد شد .
+        https://tajhizland.com/account-order-on-hold";
         $onHoldOrder = $this->onHoldOrderRepository->findOrFail($id);
         Gate::authorize("update", $onHoldOrder);
-        $this->orderRepository->setStatus($onHoldOrder->order,OrderStatus::Rejected->value);
+        $this->orderRepository->setStatus($onHoldOrder->order, OrderStatus::Rejected->value);
+        $this->smsService->send($onHoldOrder->order->orderInfo->mobile, $message);
         return $this->onHoldOrderRepository->setReject($onHoldOrder);
     }
 
