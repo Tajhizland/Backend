65983f57eeb5faee6b7571c32a471a3b5702abbf
Mehran <mehransobhani77@gmail.com>
2025-11-21 17:40:11 +0330

    CampaingBanner

--- routes/v1/admin.php
+++ routes/v1/admin.php
@@ -412,7 +412,7 @@ Route::group(["middleware" => "auth:sanctum"], function () {
         Route::delete("delete/{id}", [\App\Http\Controllers\V1\Admin\CampaignBannerController::class, "delete"]);
         Route::post("store", [\App\Http\Controllers\V1\Admin\CampaignBannerController::class, "store"]);
         Route::post("update", [\App\Http\Controllers\V1\Admin\CampaignBannerController::class, "update"]);
-        Route::get("list", [\App\Http\Controllers\V1\Admin\CampaignBannerController::class, "list"]);
+        Route::get("list/{type}", [\App\Http\Controllers\V1\Admin\CampaignBannerController::class, "list"]);
         Route::post("sort", [\App\Http\Controllers\V1\Admin\CampaignBannerController::class, "sort"]);
     });
 
