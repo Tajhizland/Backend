6874a533f92a6f9fb24fffccfb88c07a1c999c52
Mehran <mehransobhani77@gmail.com>
2025-11-24 18:22:16 +0330

    Resource

--- app/Http/Resources/V1/Discount/DiscountResource.php
+++ app/Http/Resources/V1/Discount/DiscountResource.php
@@ -5,6 +5,7 @@ namespace App\Http\Resources\V1\Discount;
 use App\Models\Discount;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Morilog\Jalali\Jalalian;
 
 /** @mixin Discount */
 class DiscountResource extends JsonResource
@@ -17,6 +18,11 @@ class DiscountResource extends JsonResource
             'status' => $this->status,
             'start_date' => $this->start_date,
             'end_date' => $this->end_date,
+
+
+            'start_date_fa' => $this->start_date != null ? Jalalian::fromDateTime($this->start_date)->format('Y/m/d H:i') : "",
+            'end_date_fa' => $this->end_date != null ? Jalalian::fromDateTime($this->end_date)->format('Y/m/d H:i') : "",
+
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
