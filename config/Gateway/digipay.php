3373f68d09b1417c98976579d1229ff22ef03e83
Mehran <mehransobhani77@gmail.com>
2026-01-28 11:28:57 +0330

    digipay

---
+++ config/Gateway/digipay.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    "client_id" =>"tajhizland67-client-id",
+    "client_secret" =>"oC5v/31eb1L,6RK",
+    "username" =>"tajhizland67",
+    "password" =>"tajhizland!",
+    'ORDER_CALLBACK_URL' => "https://tajhizland/api/v1/digipay",
+    "PURCHASE_BASE_URL" => "https://web.mydigipay.com/web-pay/tgs/",
+];
