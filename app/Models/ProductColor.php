b4aecaa11c8d894f8e9957f79cb44c2fe1acbd45
Hosseinsyd <hosseinsyd997@gmail.com>
2026-06-08 10:46:00 +0330

    fixRell

--- app/Models/ProductColor.php
+++ app/Models/ProductColor.php
@@ -46,7 +46,7 @@ class ProductColor extends Model
                     ->where(function ($subQuery3) {
                         $subQuery3->whereNull("end_date")->orWhere("end_date", ">", Carbon::now());
                     });
-            })->whereHas("product" , function ($query){
+            })->whereHas("productColor.product" , function ($query){
                 $query->where("allow_digipay", 0);
             })->latest("discount_id");
     }
