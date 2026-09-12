a054fbb65763d96b0fe8603e734c1fe4f3704055
Hosseinsyd <hosseinsyd997@gmail.com>
2026-09-12 17:03:29 +0330

    test

--- config/Gateway/snappay.php
+++ config/Gateway/snappay.php
@@ -1,11 +1,21 @@
 <?php
 return [
+//main
+//    "client_id" => "tajhiz-land",
+//    "client_secret" => "qquS0Vg45LAbx3FY",
+//    "username" => "tajhiz-land-purchase",
+//    "password" => "iy2uu44yWUy0IuhZ",
+//    'ORDER_CALLBACK_URL' => "https://api.tajhizland.com/api/v1/payment/snappay",
+//    "PURCHASE_BASE_URL" => "https://web.mydigipay.com/web-pay/tgs/",
+//    "API_BASE_URL" => "https://api.snapppay.ir",
+
+    //test
     "client_id" => "tajhiz-land",
-    "client_secret" => "qquS0Vg45LAbx3FY",
+    "client_secret" => "trrhpHUiNEXis7nh",
     "username" => "tajhiz-land-purchase",
-    "password" => "iy2uu44yWUy0IuhZ",
+    "password" => "Ylp3exzx9l05zmiW",
     'ORDER_CALLBACK_URL' => "https://api.tajhizland.com/api/v1/payment/snappay",
     "PURCHASE_BASE_URL" => "https://web.mydigipay.com/web-pay/tgs/",
     // "API_BASE_URL" => "https://uat.mydigipay.info/digipay/api",
-    "API_BASE_URL" => "https://api.snapppay.ir",
+    "API_BASE_URL" => "https://fms-gateway-staging.apps.public.okd4.teh-1.snappcloud.io",
 ];
