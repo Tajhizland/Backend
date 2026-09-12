1713cd547e1b730d186ac12d5e9cccafa5bb4bfc
Hosseinsyd <hosseinsyd997@gmail.com>
2026-09-12 16:46:19 +0330

    snappay

--- config/Gateway/snappay.php
+++ config/Gateway/snappay.php
@@ -1,11 +1,11 @@
 <?php
 return [
     "client_id" => "tajhiz-land",
-    "client_secret" => "trrhpHUiNEXis7nh",
+    "client_secret" => "qquS0Vg45LAbx3FY",
     "username" => "tajhiz-land-purchase",
-    "password" => "Ylp3exzx9l05zmiW",
+    "password" => "iy2uu44yWUy0IuhZ",
     'ORDER_CALLBACK_URL' => "https://api.tajhizland.com/api/v1/payment/snappay",
     "PURCHASE_BASE_URL" => "https://web.mydigipay.com/web-pay/tgs/",
     // "API_BASE_URL" => "https://uat.mydigipay.info/digipay/api",
-    "API_BASE_URL" => "https://fms-gateway-staging.apps.public.okd4.teh-1.snappcloud.io",
+    "API_BASE_URL" => "https://api.snapppay.ir",
 ];
