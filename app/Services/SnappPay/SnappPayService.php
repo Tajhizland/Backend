5afb0639ef2034b325093e146fb968dd88d386d0
Mehran <mehransobhani77@gmail.com>
2026-09-15 20:08:47 +0330

    snapppay

--- app/Services/SnappPay/SnappPayService.php
+++ app/Services/SnappPay/SnappPayService.php
@@ -46,7 +46,7 @@ class SnappPayService
                 'username' => $username,
                 'password' => $password,
             ]);
-        var_dump($response->getStatusCode());
+        var_dump($response->throw());
         die();
         $this->logCall("snapppay.auth", ["grant_type" => "password", "scope" => "online-merchant", "username" => $username, "password" => "***", "client_id" => $client_id], $response);
 
